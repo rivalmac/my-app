634c0773e1e3bcd2085d20a0f5f314d325acaf09
Jason Macauley <rivalmac@gmail.com>
2018-11-29 15:26:47 -0500

    initial add
    foo

---
+++ public/like_button.js
@@ -0,0 +1,34 @@
+/**
+ * Created by jasonmacauley on 11/29/18.
+ */
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { liked: false };
+    }
+
+    render() {
+        if (this.state.liked) {
+            return 'You liked comment ' + this.props.commentID;
+        }
+
+        return e(
+            'button',
+            { onClick: () => this.setState( { liked: true } ) },
+            'Like'
+        );
+    }
+}
+
+
+document.querySelectorAll(' #like_button_container').forEach(domContainer => {
+    const commentID = parseInt(domContainer.dataset.commentid);
+    ReactDOM.render(
+        e(LikeButton, { commentID: commentID}),
+        domContainer
+    ); }
+);
